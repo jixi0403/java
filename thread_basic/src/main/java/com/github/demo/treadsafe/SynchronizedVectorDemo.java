e609e6bb080181e7ac2cd31e4d9b9dbd3b8660ac
小四 <errorfatal89@gmail.com>
2017-03-12 23:40:57 +0800

    使用线程安全容器进行复合操作时线程安全问题

---
+++ thread_basic/src/main/java/com/github/demo/treadsafe/SynchronizedVectorDemo.java
@@ -0,0 +1,30 @@
+package com.github.demo.threadsafe;
+
+public class SynchronizedVectorDemo {
+
+	@ThreadSafe
+	public static Object getLast(Vector list) {
+		synchronized(list) {
+			int lastIndex = list.size() - 1;
+			return list.get(lastIndex);
+		}
+	}
+
+	@ThreadSafe
+	public static void deleteLast(Vector list) {
+		synchronized(list) {
+			int lastIndex = list.size()) - 1;
+			return list.remove(lastIndex);
+		}
+	}
+
+	public static Object getLastNotThreadSafe(Vector list) {
+		int lastIndex = list.size() - 1;
+		return list.get(lastIndex);
+	}
+
+	public static void deleteLastNotThreadSafe(Vector list) {
+		int lastIndex = list.size() - 1;
+		list.remove(lastIndex);
+	}
+}
