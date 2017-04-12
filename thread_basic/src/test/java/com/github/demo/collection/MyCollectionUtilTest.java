4a0e041727484d8815686ec47651733a9926961d
jixi <601927342@qq.com>
2017-04-12 14:29:47 +0800

    collection 基本使用用例

--- thread_basic/src/test/java/com/github/demo/collection/MyCollectionUtilTest.java
+++ thread_basic/src/test/java/com/github/demo/collection/MyCollectionUtilTest.java
@@ -1,12 +1,10 @@
 package com.github.demo.collection;
 
 import com.github.collection.MyCollectionUtil;
+import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by Sunshine_wx on 17/4/12.
@@ -21,4 +19,33 @@ public class MyCollectionUtilTest {
         MyCollectionUtil.doSomething(set);
         MyCollectionUtil.doSomething(list);
     }
+
+    @Test
+    public void testAddAndRemove() {
+        String aElement = "an element";
+        Collection collection = new HashSet();
+        boolean didCollectionChange = collection.add(aElement);
+        Assert.assertTrue(didCollectionChange);
+        boolean wasElementRemoved = collection.remove(aElement);
+        Assert.assertTrue(wasElementRemoved);
+    }
+
+    @Test
+    public void testCollectionSize() {
+        Collection set = new HashSet();
+        set.add("a");
+
+        Assert.assertEquals(1, set.size());
+    }
+
+    @Test
+    public void testNewLoop() {
+        Collection set = new HashSet();
+        set.add("a");
+
+        for(Object object : set) {
+            System.out.println(object);
+        }
+    }
+
 }
